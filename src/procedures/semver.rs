79ef1bcbf70cb536bd0e3a751b911f1cf830488f
Simon <simon@kovtyk.com>
2026-05-24 17:37:53 +0200

    fix(semver): apply merge on configured semver with bumped one

--- src/procedures/semver.rs
+++ src/procedures/semver.rs
@@ -1,4 +1,4 @@
-use crate::{config::Config, log::log_debug, semver::{core::SemVer, r#type::SemVerType}};
+use crate::{config::Config, log::log_debug, semver::{core::SemVer, r#type::SemVerType}, std::merge::Merge};
 
 pub struct GetSemVerResult {
   pub semver: SemVer
@@ -10,25 +10,21 @@ pub fn get_semver (
 ) -> GetSemVerResult {
   let config = Config::inject();
 
-  let config_semver = config.semver.clone().map(|v| v.to_semver_with_format()).flatten();
-
-  if let Some(inner_config_semver) = config_semver && inner_config_semver.is_fullfilled() {
-    log_debug(
-      &format!("Using SemVer from config: {:?}", &inner_config_semver)
-    );
-
-    return GetSemVerResult {
-      semver: inner_config_semver
-    };
-  }
-
   let base_semver = if let Some(inner_latest_semver) = latest_semver && inner_latest_semver.is_fullfilled() {
     inner_latest_semver
   } else {
     SemVer::default()
   };
 
-  let semver = base_semver.bump(semver_type);
+  let mut semver = base_semver.bump(semver_type);
+
+  let config_semver = config.semver.clone().map(|v| v.to_semver_with_format()).flatten();
+
+  if let Some(inner_config_semver) = config_semver {
+    semver = inner_config_semver.merge(
+      semver
+    );
+  }
 
   log_debug(
     &format!(
